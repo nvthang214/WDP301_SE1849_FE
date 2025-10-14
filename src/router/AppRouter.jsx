db4e54eb7b383ab3d4fec46ab0239dfc218a0ad3
Tuong Xuan Luong <133077787+xuanluongtuong@users.noreply.github.com>
2025-10-14 22:59:22 +0700

    fix app router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -89,6 +89,14 @@ const routes = [
     path: ROUTER.COMPANY_INFO,
     element: (
       <LazyLoadingComponent>
+        <CompanyInfo />
+      </LazyLoadingComponent>
+    ),
+  },
+  {
+    path: ROUTER.COMPANY_DETAILS,
+    element: (
+      <LazyLoadingComponent>
         <CompanyInformation />
       </LazyLoadingComponent>
     ),
